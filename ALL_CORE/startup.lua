424b0573fb4c57d2960e8c29684d5074bf1686f6
2 1 <InotitoKokoro@gmail.com>
2024-10-06 20:44:31 +0900

    webhookテストその2

--- ALL_CORE/startup.lua
+++ ALL_CORE/startup.lua
@@ -1,1 +1,7 @@
 print("MameeennArea System")
+
+--#region Peripherals
+---@type Modem
+local modem = peripheral.find("modem", function(name, modem) return modem.isWireless() end)
+if not modem then error("Wireless modem not connected") end
+--#endregion
