cd7614d33c22bd7964b88bea25e6c391da9623fb
2 1 <InotitoKokoro@gmail.com>
2024-10-06 16:49:31 +0900

    改札タートルの仕様変更・config追加

---
+++ GateTurtle/configuration.lua
@@ -0,0 +1,56 @@
+local GUIFile = require("gui")
+local GUI = GUIFile.GUI;
+local Potion = GUIFile.Potion;
+local termX = term.getSize();
+local function getCenter(char) return (termX - char) / 2; end
+local config = {
+    stationName = "",
+    --ぶっちゃけ何でもいい
+    port = 64345
+}
+
+local function setConfig()
+    ---@type WriteStream
+    local stream = fs.open("_config.json", "w")
+    stream.write(textutils.serialiseJSON(config));
+    stream.close();
+end
+if fs.exists("_config.json") then
+    ---@type ReadStream
+    local stream = fs.open("_config.json", "r")
+    config = textutils.unserialiseJSON(stream.readAll());
+    stream.close();
+else
+    setConfig()
+end
+
+term.setPaletteColor(colors.brown, 0x2b2c42)
+term.setBackgroundColor(colors.brown)
+Potion.termSize():clear()
+GUI.Text(Potion.newVector2(getCenter(15), 1), "Station  Config")
+GUI.Text(Potion.newVector2(1, 3), "Station Name")
+GUI.Text(Potion.newVector2(1, 5), "Port(default:64345)")
+local exitCenterPos = (" "):rep(getCenter(4) + 1);
+local Buttons = {
+    GUI.Input(Potion.newSize(15, 3, 40, 3), config.stationName, colors.gray, colors.white, false, nil,
+        function(value)
+            config.stationName = value;
+            setConfig();
+        end),
+    GUI.Input(Potion.newSize(22, 5, 27, 5), tostring(config.port), colors.gray, colors.white, false, nil,
+        function(value)
+            if tonumber(value) then
+                config.port = tonumber(value);
+                setConfig();
+            end
+        end),
+    GUI.Button(Potion.newSize(1, 19, 51, 19), exitCenterPos .. "Exit" .. exitCenterPos, colors.green, colors.white,
+        function()
+            term.setBackgroundColor(colors.black);
+            Potion.termSize():clear();
+            term.setCursorPos(1, 1);
+            return "exit";
+        end)
+}
+
+GUI.WaitForAllButtonOrInput(table.unpack(Buttons))
--- GateTurtle/startup.lua
+++ GateTurtle/startup.lua
@@ -1,21 +1,106 @@
+if not fs.exists("_config.json") then
+    shell.run("configuration")
+    return;
+end
+local POSITION_SHIFT = {
+    north = { 0, 0, -1 },
+    east = { 1, 0, 0 },
+    south = { 0, 0, 1 },
+    west = { -1, 0, 0 }
+}
+
+--#region Peripherals
+---@type Modem
+local modem = peripheral.find("modem", function(name, modem) return modem.isWireless() end)
+if not modem then error("Wireless modem not connected") end
+---@type PlayerDetector
+local playerDetector = peripheral.find("playerDetector")
+if not playerDetector then error("Player Detector not connected") end
+--#endregion
+
+---@type ReadStream
+local readStream = fs.open("_config.json", "r")
+local config = textutils.unserialiseJSON(readStream.readAll());
+readStream.close();
+
+local position = { gps.locate() };
+local gatePosition = position;
+for i, _ in ipairs(position) do
+    gatePosition[i] = gatePosition[i] + POSITION_SHIFT[config.side][i]
+end
+
+local playerPassingPositionFrom = {};
+local playerPassingPositionTo = {};
+
+if config.side == "south" or config.side == "north" then
+    playerPassingPositionFrom = {
+        gatePosition[1] - 1,
+        gatePosition[2] - 0.5,
+        gatePosition[3] - 0.25,
+    }
+    playerPassingPositionTo = {
+        gatePosition[1] + 2,
+        gatePosition[2] + 1,
+        gatePosition[3] + 1,
+    }
+else
+    playerPassingPositionFrom = {
+        gatePosition[1] - 0.25,
+        gatePosition[2] - 0.5,
+        gatePosition[3] - 1,
+    }
+    playerPassingPositionTo = {
+        gatePosition[1] + 1,
+        gatePosition[2] + 1,
+        gatePosition[3] + 2,
+    }
+end
+
+local function arrayToPosition(array)
+    local KEYS = { "x", "y", "z" }
+    local positionObject = {};
+    for index, value in ipairs(array) do
+        positionObject[KEYS[index]] = value;
+    end
+    return positionObject;
+end
+playerPassingPositionFrom = arrayToPosition(playerPassingPositionFrom)
+playerPassingPositionTo = arrayToPosition(playerPassingPositionTo)
+
 local function OpenGate()
     redstone.setOutput("front", true)
     turtle.dropDown();
-    sleep(0.75);
-    redstone.setOutput("front", false)
 end
 local function Main()
     repeat
-        turtle.suckUp();
-        if turtle.getItemCount() ~= 0 then
-            local item = turtle.getItemDetail();
-            if item.name == "lightmanscurrency:coin_copper" then
-                OpenGate()
-            else
-                turtle.turnLeft()
-                turtle.drop();
-                turtle.turnRight();
-                sleep(1);
+        local players = playerDetector.getPlayersInCoords(playerPassingPositionFrom, playerPassingPositionTo);
+        if #players == 1 then
+            turtle.suckUp(1);
+            if turtle.getItemCount() ~= 0 then
+                local item = turtle.getItemDetail();
+                if item.name == "lightmanscurrency:coin_copper" then
+                    local payload = {
+                        position = position,
+                        player = players[1],
+                        go_inside = config.inside,
+                        payed = "1c",
+                        from = "gate"
+                    }
+                    modem.transmit(config.port, 0, payload)
+                    OpenGate()
+                    repeat
+                        players = playerDetector.getPlayersInCoords(playerPassingPositionFrom, playerPassingPositionTo);
+                        sleep(0.05)
+                    until #players ~= 1
+                    redstone.setOutput("front", false)
+                else
+                    turtle.drop();
+                    sleep(1);
+                end
+            end
+        else
+            if redstone.getOutput("front") then
+                redstone.setOutput("front", false)
             end
         end
     until false
