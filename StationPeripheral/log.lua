ed9e6a835f00a225e4a85e65109870087c57fc02
2 1 <InotitoKokoro@gmail.com>
2024-10-06 16:51:02 +0900

    改札システムを軽く実装

--- StationPeripheral/log.lua
+++ StationPeripheral/log.lua
@@ -1,9 +1,9 @@
 local termX, termY = term.getSize();
-return function(taskName, color)
+return function(taskName, taskColor)
     ---@class LogPrinter
     local logPrinter = {}
     logPrinter.taskName = taskName;
-    logPrinter.taskColor = color;
+    logPrinter.taskColor = taskColor;
     function logPrinter.print(self, text)
         local time = os.date("%H:%M:%S")
         term.blit("[" .. time .. ",", "0bbbbbbbb0", "ffffffffff")
@@ -13,22 +13,52 @@ return function(taskName, color)
         term.write("] ")
         local splited = {};
         local remainingTexts = text;
-        repeat
-            local writedCount = term.getCursorPos();
-            local writeable = termX - writedCount;
-            print(remainingTexts:sub(0, writeable))
-            remainingTexts = remainingTexts:sub(writeable + 1)
-            if #remainingTexts == 0 then
-                break;
+        local writedCount = term.getCursorPos();
+        local writeable = termX - writedCount;
+        --local writeText = remainingTexts:sub(0, writeable);
+        local continue = false;
+        for i = 1, #text, 1 do
+            local char = text:sub(i, i)
+            local nextChar = text:sub(i + 1, i + 1)
+            if continue then
+                continue = false;
+            elseif nextChar == "\n" or #nextChar == 0 or ({ term.getCursorPos() })[1] == termX then
+                _G.print(char);
+            elseif char == "\222" then
+                local color = colors.fromBlit(nextChar)
+                term.setTextColor(color)
+                continue = true;
             else
-                term.blit("\149 ", "70", "ff")
+                if ({ term.getCursorPos() })[1] == 1 then
+                    term.blit("\149 ", "70", "ff")
+                end
+                term.write(char)
             end
-        until false
+        end
+        remainingTexts = remainingTexts:sub(writeable + 1)
     end
 
     function logPrinter.AddPrint(self, text)
         term.blit("\149 ", "70", "ff")
-        print(text)
+        local continue = false;
+        for i = 1, #text, 1 do
+            local char = text:sub(i, i)
+            local nextChar = text:sub(i + 1, i + 1)
+            if continue then
+                continue = false;
+            elseif nextChar == "\n" or #nextChar == 0 or ({ term.getCursorPos() })[1] == termX then
+                _G.print(char);
+            elseif char == "\222" then
+                local color = colors.fromBlit(nextChar)
+                term.setTextColor(color)
+                continue = true;
+            else
+                if ({ term.getCursorPos() })[1] == 1 then
+                    term.blit("\149 ", "70", "ff")
+                end
+                term.write(char)
+            end
+        end
     end
 
     return logPrinter;
--- StationPeripheral/tasks/gate.lua
+++ StationPeripheral/tasks/gate.lua
@@ -1,4 +1,25 @@
 local log = require("log")("Gate")
-return function()
+
+--#region Peripherals
+---@type Modem
+local modem = peripheral.find("modem", function(name, modem) return modem.isWireless() end)
+if not modem then error("Wireless modem not connected") end
+---@type ChatBox
+local chatBox = peripheral.find("chatBox")
+if not chatBox then error("ChatBox not connected") end
+--#endregion
+return function(config)
     log:print("activated.")
+    modem.open(config.port)
+    repeat
+        ---@type ["modem_message",string,number,number,table,number]
+        local message = { os.pullEvent("modem_message") };
+        if message[5].from == "gate" then
+            ---@type {position:number[],player:string,go_inside:boolean,payed:string}
+            local payload = message[5];
+            log:print("Pay report:" .. payload.player .. ": " .. payload.payed)
+            chatBox.sendMessageToPlayer("Payed in " .. config.stationName .. ", amount: " .. payload.payed .. "",
+                payload.player, "&3Station&r");
+        end
+    until false
 end
