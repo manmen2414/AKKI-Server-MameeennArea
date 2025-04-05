06bbee4e865762f32c6e4dc563ecf8a7e1f3eb7a
manmen2414 <inotitokokoro@gmail.com>
2025-04-06 00:22:29 +0900

    アイテムが消えるバグの一時対処

--- April_2025/mermaid_frogport/startup.lua
+++ April_2025/mermaid_frogport/startup.lua
@@ -1,12 +1,13 @@
 local ids = {
-  frogport = "create:package_frogport_0",
   sendPackager = "create:packager_0",
   receivePackager = "create:packager_1"
 }
-local frogport = peripheral.wrap(ids.frogport);
+local DEFAULT_FROGPORT = 1;
+local frogports = { peripheral.find("create:package_frogport") };
 local sendPackager = peripheral.wrap(ids.sendPackager);
 local receivePackager = peripheral.wrap(ids.receivePackager);
 local sendingCardboard = {};
+if #frogports == 0 then error("frogport not connected"); end
 
 local function writeStart()
   term.clear();
@@ -25,10 +26,11 @@ local function sendPackagerTask()
     local OPackagerList = sendPackager.list();
     if #OPackagerList ~= 0 then
       for index, item in ipairs(OPackagerList) do
-        local sucessed = sendPackager.pushItems(ids.frogport, index)
+        local sucessed = sendPackager.pushItems(
+          peripheral.getName(frogports[DEFAULT_FROGPORT]), index)
         sendingCardboard[item.nbt] = true;
         if sucessed > 0 then
-          writeLog("[SEND] " .. item.nbt, colors.yellow)
+          writeLog("[SEND " .. DEFAULT_FROGPORT .. "] " .. item.nbt, colors.yellow)
         end
       end
     end
@@ -36,18 +38,23 @@ local function sendPackagerTask()
 end
 local function receivePackagerTask()
   repeat
-    local frogportList = frogport.list();
-    if #frogportList ~= 0 then
-      for index, item in ipairs(frogportList) do
-        if not sendingCardboard[item.nbt] then
-          local sucessed = frogport.pushItems(ids.receivePackager, index)
-          if sucessed > 0 then
-            writeLog("[RECV] " .. item.nbt, colors.green)
+    for frogIndex, frogport in ipairs(frogports) do
+      local frogportList = frogport.list();
+      if #frogportList ~= 0 then
+        for index, item in ipairs(frogportList) do
+          if not sendingCardboard[item.nbt] then
+            local sucessed = frogport.pushItems(ids.receivePackager, index)
+            if sucessed > 0 then
+              writeLog("[RECV " .. frogIndex .. "] " .. item.nbt, colors.green)
+            end
+            --パッケージャーが空になるまでの時間。余裕をもって1
+            --//TODO: シングルプレイワールドでバグの整合性を確認する
+            sleep(1)
           end
         end
+      else
+        sendingCardboard = {};
       end
-    else
-      sendingCardboard = {};
     end
   until false
 end
